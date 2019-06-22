97288215858e8965602ccfdf64bd05d2e4a0413f
Kei Man Chau <chkeiman@hk1.ibm.com>
2019-06-23 02:53:40 +0800

    Changed folder structure

--- MavenDependencyGenerator/src/main/java/IClasspathFileReader.java
+++
@@ -1,9 +0,0 @@
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.util.List;
-
-public interface IClasspathFileReader {
-    List<String> read(String filePath) throws ParserConfigurationException, IOException, SAXException;
-}
