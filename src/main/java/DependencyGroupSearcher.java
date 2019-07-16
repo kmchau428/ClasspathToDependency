3dd3354dd7c67ba4ff7fc36b861d247b34c9ac41
Kei Man Chau <chkeiman@hk1.ibm.com>
2019-07-16 14:22:32 +0800

    added support of multuple groupIds

--- src/main/java/DependencyGroupSearcher.java
+++ src/main/java/DependencyGroupSearcher.java
@@ -13,8 +13,8 @@ public class DependencyGroupSearcher {
     public static String findGroupId(String artifactId, String version) {
         Client client = Client.create();
 
-        String endpoint = "https://search.maven.org/solrsearch/select?q=a:%22" + artifactId + "%22%20AND%20v:%22" + version + "%22&wt=json";
-//        String endpoint = "https://search.maven.org/solrsearch/select?q=a:%22" + "maven-ear-plugin" + "%22&wt=json";
+//        String endpoint = "https://search.maven.org/solrsearch/select?q=a:%22" + artifactId + "%22%20AND%20v:%22" + version + "%22&wt=json";
+        String endpoint = "https://search.maven.org/solrsearch/select?q=a:%22" + "maven-ear-plugin" + "%22&wt=json";
         System.out.println("resolving the groupId: " + endpoint);
         WebResource webResource = client.resource(endpoint);
 
@@ -24,28 +24,34 @@ public class DependencyGroupSearcher {
                 .toString();
 
         String groupId = "unclassified";
-        int resultIdx = resp.indexOf("docs=");
-        String[] results = resp.substring(resultIdx + "docs=".length(), resp.indexOf("}]", resultIdx)).split("},");
-        if (results.length > 1) {
-            String groupIdCandidates = "";
-
-            for (String s : results) {
-                int groupIdIdx = s.indexOf("g=");
-                groupIdCandidates += s.substring(groupIdIdx+2, s.indexOf(",",groupIdIdx)) + "\n";
-            }
-            System.out.println("Multiple groupIds found: please choose one from:\n" + groupIdCandidates);
-            Scanner reader = new Scanner(System.in);
-            System.out.println("Enter the groupId name: ");
-            groupId = reader.next();
-
+        int resultIdxStart = resp.indexOf("docs=");
+        int resultIdxEnd = resp.indexOf("}]", resultIdxStart);
+        if (resultIdxEnd < 0) { //not found
+            System.out.println("The groupId of the jar " + artifactId + "-" + version + " cannot be found");
         }
         else {
-            int groupIdIdx = resp.indexOf("g=");
-            if (groupIdIdx < 0) { //not found
-                System.out.println("The groupId of the jar " + artifactId + "-" + version + " cannot be found");
+            String[] results = resp.substring(resultIdxStart + "docs=".length(), resp.indexOf("}]", resultIdxStart)).split("},");
+            if (results.length > 1) {
+                String groupIdCandidates = "";
+
+                for (String s : results) {
+                    int groupIdIdx = s.indexOf("g=");
+                    groupIdCandidates += s.substring(groupIdIdx+2, s.indexOf(",",groupIdIdx)) + "\n";
+                }
+                System.out.println("Multiple groupIds found: please choose one from:\n" + groupIdCandidates);
+                Scanner reader = new Scanner(System.in);
+                System.out.println("Enter the groupId name: ");
+                groupId = reader.next();
+
             }
             else {
-                groupId = resp.substring(groupIdIdx+2, resp.indexOf(",",groupIdIdx));
+                int groupIdIdx = resp.indexOf("g=");
+                if (groupIdIdx < 0) { //not found
+                    System.out.println("The groupId of the jar " + artifactId + "-" + version + " cannot be found");
+                }
+                else {
+                    groupId = resp.substring(groupIdIdx+2, resp.indexOf(",",groupIdIdx));
+                }
             }
         }
 
